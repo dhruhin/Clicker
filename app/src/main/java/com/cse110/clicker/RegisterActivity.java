03a870a9cb66bc2eca8d72aa9b0cdc2094b6df3f
Dhruhin Kurli <dhruhin@gmail.com>
2015-10-26 17:51:48 -0700

    Fixed Intent Issue with Register Screen

---
+++ app/src/main/java/com/cse110/clicker/RegisterActivity.java
@@ -0,0 +1,73 @@
+package com.cse110.clicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.widget.EditText;
+import android.widget.Button;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText first, last, email, password, confirmPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Firebase.setAndroidContext(this);
+        //comment
+        setContentView(R.layout.activity_login);
+        first = (EditText) findViewById(R.id.editFirst);
+        last = (EditText) findViewById(R.id.editLast);
+        email = (EditText) findViewById(R.id.editEmail);
+        password = (EditText) findViewById(R.id.editPassword);
+        confirmPassword = (EditText) findViewById(R.id.editConfirmPassword);
+        Button register = (Button)findViewById(R.id.registerButton);
+        register.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                attemptLogin();
+            }
+        });
+    }
+    private void attemptLogin(){
+        Log.d("a", "b");
+
+        if(password.getText().toString().equals("")){
+            //Password is blank
+            return;
+        }
+
+        if(!password.getText().toString().equals(confirmPassword.getText().toString())){
+            //Passwords do not match
+            return;
+        }
+        String semail = email.getText().toString();
+        String spassword = password.getText().toString();
+        String sfirst = first.getText().toString();
+        String slast = last.getText().toString();
+
+        Firebase ref = new Firebase("https://cse110clicker.firebaseio.com");
+        ref.createUser(semail, spassword, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                System.out.println("Successfully created user account with uid: " + result.get("uid"));
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                // there was an error
+
+            }
+        });
+    }
+}
