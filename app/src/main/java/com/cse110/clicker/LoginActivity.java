2172d2fb0387b8a59dcac5b86af52acba2f64fbd
Dhruhin Kurli <dhruhin@gmail.com>
2015-10-27 09:34:50 -0700

    Added session adding

--- app/src/main/java/com/cse110/clicker/LoginActivity.java
+++ app/src/main/java/com/cse110/clicker/LoginActivity.java
@@ -27,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Firebase.setAndroidContext(this);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
     }
--- app/src/main/java/com/cse110/clicker/RegisterActivity.java
+++ app/src/main/java/com/cse110/clicker/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.cse110.clicker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,43 +18,77 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText first, last, email, password, confirmPassword;
+    private EditText first, last, email, password, confirmPassword, schoolID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Firebase.setAndroidContext(this);
         first = (EditText) findViewById(R.id.editFirst);
         last = (EditText) findViewById(R.id.editLast);
         email = (EditText) findViewById(R.id.editEmail);
         password = (EditText) findViewById(R.id.editPassword);
         confirmPassword = (EditText) findViewById(R.id.editConfirmPassword);
+        schoolID = (EditText) findViewById(R.id.editID);
     }
     public void register(View view){
-        Log.d("a", "b");
-
-        if(!password.getText().toString().equals(confirmPassword.getText().toString())){
-            //Passwords do not match
+        final String semail = email.getText().toString();
+        final String spassword = password.getText().toString();
+        final String scpassword = confirmPassword.getText().toString();
+        final String sfirst = first.getText().toString();
+        final String slast = last.getText().toString();
+        final String sid = schoolID.getText().toString();
+        if(semail.isEmpty() || spassword.isEmpty() || sfirst.isEmpty() || slast.isEmpty()){
+            alertError("Not all required fields are filled out.");
             return;
         }
-        String semail = email.getText().toString();
-        String spassword = password.getText().toString();
-        String sfirst = first.getText().toString();
-        String slast = last.getText().toString();
+        if(!spassword.equals(scpassword)){
+            return;
+        }
 
-        Firebase ref = new Firebase("https://cse110clicker.firebaseio.com");
+        final Firebase ref = new Firebase("https://cse110clicker.firebaseio.com");
         ref.createUser(semail, spassword, new Firebase.ValueResultHandler<Map<String, Object>>() {
             @Override
             public void onSuccess(Map<String, Object> result) {
                 System.out.println("Successfully created user account with uid: " + result.get("uid"));
+                Firebase user = ref.child("users").child((String)result.get("uid"));
+                user.child("first").setValue(sfirst);
+                user.child("last").setValue(slast);
+                user.child("id").setValue(sid);
+                //stored
+                ref.authWithPassword(semail, spassword, new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        startActivity(new Intent(RegisterActivity.this, DashboardActivity.class));
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        // there was an error
+                        alertError("Unable to Login, try again");
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }
+                });
             }
 
             @Override
             public void onError(FirebaseError firebaseError) {
                 // there was an error
-
+                alertError("Registration Error!");
             }
         });
+    }
+
+    public void alertError(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //do nothing when clicked
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
